5dc0685a5a7169d42a54e2dc5273886c2dbe1dde
PierTwo <105090994+PierTwo@users.noreply.github.com>
2022-07-09 07:15:39 -0500

    Created questions for user to answer and added pseudocode

---
+++ assets/js/questions.js
@@ -0,0 +1,37 @@
+var questions = [
+    {
+        question: "What is NOT a common data type?",
+        choices: ["string", "boolean", "while", "undefined"],
+        answer: "while"
+    },
+    {
+        question: "Which of these represents strict equality?",
+        choices: [">=", "===", "&&", "=="],
+        answer: "==="
+    },
+    {
+        question: "Which of these will loop through a block of code a specified number of times?",
+        choices: ["for", "else", "while", "switch"],
+        answer: "for"
+    },
+    {
+        question: "What number represents the third item in an array?",
+        choices: ["3", "1", "2", "4"],
+        answer: "2"
+    },
+    {
+        question: "Which of these is used to declare a variable?",
+        choices: ["if", "var", "for", "else if"],
+        answer: "var"
+    },
+    {
+        question: "Which of these is a statement that can be used when an if statement is false?",
+        choices: ["next", "if false", "for", "else"],
+        answer: "else"
+    },
+    {
+        question: "What operator is used when assigning a variable its value",
+        choices: ["=", "?", "===", "!="],
+        answer: "="
+    }
+];
---
+++ docs/PSEUDOCODE.md
@@ -0,0 +1,22 @@
+# Step 1
+
+1. A link to the high scores that takes me to view the high score leaderboard
+2. A timer that shows the time remaining once the game has started 
+3. When I click on the start button an event listener will run the code to start the game
+4. Dynamically render the questions on the screen
+5. Dynamically render the answers on the screen
+6. Use a container in the HTML to put questions and answers in
+7. Use prevent default to stop buttons from refreshing the page
+8. Create an array of questions and answers
+9. Add listener to container to element.match the buttons and run the the code to compare the answer
+10. If user is correct show next question and if incorrect decrease the timer
+11. If timer hits zero end the game
+12. If there are no more questions end the game
+13. Have user enter initials to place score on leaderboard
+
+# Step 2
+
+1. When user is taken to highscore page show the leaderboard 
+2. Sort by highest score first
+3. Add button to clear leaderboard
+4. Be able to return to quiz page 
